e1ca1e4308ca493e02e825493caeaaff2aa3877b
L Angell <lonny.angell@gmail.com>
2026-01-14 18:02:32 -0600

    fix: updated Auth client

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 
-export default NextAuth(authConfig).auth;
+export default NextAuth({
+    ...authConfig,
+    trustHost: true,
+}).auth;
 
 export const config = {
     matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
